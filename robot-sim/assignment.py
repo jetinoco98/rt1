0813bd889ba5728935ee40fb21da2bb45f84200b
Josue <jetinoco98@hotmail.com>
2024-02-12 00:11:49 +0100

    Complete re-work of code

--- robot-sim/assignment.py
+++ robot-sim/assignment.py
@@ -5,25 +5,29 @@ from sr.robot import *
 
 R = Robot()
 
-
 #### DECLARATION OF GLOBAL VARIABLES
-
 ''' Threshold for the control of the orientation'''
 a_th = 2.0
 '''Threshold for the control of the linear distance'''
 d_th = 0.4
-'''List of IDs of boxes that have been put together'''
-primel = []
 '''An arbitrary maximum speed for the robot'''
 max_speed = 100
 '''A relative low speed for the robot'''
 low_speed = 20
+''' A boolean state to show the grab status'''
+has_box = False
 
-grabbed = False
+# New Variables
+robot_vision = {}  #  Code: (distance, angle)
+boxes = []  # All boxes in the window
+boxes_at_goal = []  # All boxes that have been placed together
 
-#### FUNCTIONS FOR THE CODE EXECUTION
 
-def drive(speed, seconds):
+########################
+# FUNCTIONS
+########################
+
+def drive(speed, seconds=0):
     """
     Function for setting a linear velocity. 
     If the seconds are 0, the linear velocity will remain.
@@ -37,7 +41,7 @@ def drive(speed, seconds):
         R.motors[0].m0.power = 0
         R.motors[0].m1.power = 0
 
-def turn(speed, seconds):
+def turn(speed, seconds=0):
     """
     Function for setting an angular velocity. 
     If the seconds are 0, the angular velocity will remain.
@@ -52,181 +56,200 @@ def turn(speed, seconds):
 
 def stop():
     '''Stop the robot. That's it.'''
-    drive(0,0)
-    turn(0,0)
+    drive(0)
+    turn(0)
 
-def obtain_speed(distance):
-    ''' A function so that when the robot reaches the selected distance of 1
-    with respect to a box, its velocity will decrease to the low value'''
-    # The distance of 0.9 does not represent any special calculated value, but just a treshold chosen by internal testing.
+def speed_selector(distance):
+    ''' Velocity depends on the distance to the goal box'''
+    speed = 0
     if distance > 0.9:
         speed = max_speed
-    else:
+    elif d_th < distance <= 0.9:
         speed = low_speed
     return speed
 
-def special_turn(i):
-    # Robot looks to the near left
-    if 0<i<10:
-        turn(-50,0.05)
-    # Robot looks to the near right
-    elif 10<i<30:
-        turn(50,0.05)
-    # Robot keeps turning clockwise.
-    else:
-        turn(50,0.1)
+def update_vision_data():
+    global robot_vision
+    robot_vision = {}
+    markers = [marker for marker in R.see()]
+    for m in markers:
+        code = m.info.code
+        distance = m.dist
+        rot = m.rot_y
+        robot_vision[code] = [distance, rot]
 
-def find_closest_box():
-    '''As the name suggests, this function is used to find the closest box that the robot is 
-        able to see with its integrated vision system (the R.see() function)'''
-    codel = []  # List of all the boxes codes found
-    distl = []  # List of all distances found
-    i = 0
-    while 1:
+def initial_scan():
+    '''This function will try to scan the data from all the boxes in the arena'''
+    boxes_dict = {} # List of the data for all the boxes found
+    for i in range(11):
         # Obtaining all markers in the robot's view
-        raw_markers = R.see()
-        markers = [marker for marker in raw_markers if marker.info.code not in primel]
-
-        #If at least one marker is found, then it should be the closest box.
-        if len(markers)>0:
-            # In case the code in else was executed first, we should stop the robot from turning.
-            turn(0,0)
-            # We iterate through the markers list and extract all boxes codes and it corresponding distances.
-            for m in markers:
-                box_code = m.info.code
-                box_distance = m.dist
-                codel.append(box_code)
-                distl.append(box_distance)
-
-            # The minimum values obtainted from the lists represent the closest found box.
-            min_dist = min(distl)
-            min_dist_index = distl.index(min_dist)
-            code = codel[min_dist_index]
-
-            # The value of the code that represents the closest box will be used for the go_grab function.
-            return code
-        
-        # If the robot cannot see any marker, it will begin to turn.
+        markers = [marker for marker in R.see()]
+        # SCAN the environment with 2 sweeps (left and right)
+        if i < 4:
+            turn(10)
         else:
-            turn(50,0.05)
-            # If after a calculated number iterations (robot turns >360°) the robot cannot find any box... 
-            # ...it's because they are all grouped together abd the robot's job is complete.
-            if i>60:
-                turn(0,0)
-                print('Could not find any more boxes in the Arena.')
-                print('PROGRAM HAS ENDED')
-                exit()
-            i+=1
-
-def find_prime_box(codel):
-    i = 0
-    while True:
-        # It was discovered that it's not uncommon for the robot to lose vision of the prime box despite being in front.
-        # A special series of turning actions was coded inside the special_turn() function.
-        special_turn(i)
-        markers = R.see()
+            turn(-10)
+        # We iterate through the markers list and extract all boxes codes and it corresponding distances.
         for m in markers:
-            if m.info.code in codel:
-                stop()
-                return m.info.code
-        i+=1 
+            code = m.info.code
+            distance = m.dist
+            angle = m.rot_y
+            if code not in boxes_dict:
+                boxes_dict[code] = (distance, angle)
+        time.sleep(0.5)
+        i+=1
+    # After the for loop ends
+    stop()
+    return boxes_dict
 
-def box_data(code):
-    '''This function uses the code of the box that the robot must go to, in order to extract the distance and rotation
-        values obtained from the robot's vision system (the R.see() function)'''
-    while 1:
-        try:
-            markers = R.see()
-            for m in markers:
-                if m.info.code == code:
-                    box_distance = m.dist
-                    box_rot = m.rot_y
-            return box_distance, box_rot
-        except:
-            print('Error in box data while looking for box with code: ', code)
-            break
+def find_closest_box(codes):
+    ''' This function finds the closest box from the robot's vision system.
+        If a code list (codes) is given, it will filter the search.'''
+    global robot_vision, boxes_at_goal, has_box
 
-def go(dist,rot):
-    '''The go function moves the robot towards the specified box.'''
-    # If the angle exceeds the threshold, it will reorient itself
-    time.sleep(0.1)
-    if rot < -a_th:
-        turn(-5, 0)
-        print('Changing Rot',dist, rot)
-    elif rot > a_th:
-        turn(5, 0)
-        print('Changing Rot',dist, rot) 
-    # Else, it will drive at a certain speed to the box
-    else:
-        turn(0,0)
-        speed = obtain_speed(dist)
-        drive(speed,0)
-        print('Changing dist',dist, rot)
+    start_time = time.time()
+    timeout_seconds = 5
 
-def go_grab(code):
-    global grabbed
-    print('Executing the Grab function.')
-    while 1:
-        dist, rot = box_data(code)
-        go(dist,rot)
-        if dist <= d_th:
-            stop()
-            R.grab()
-            grabbed = True
-            break
+    filtered_robot_vision = {}
 
-def go_release(prime_code, code):
-    global grabbed
-    print('Executing the Release function')
+    # Loops ends until box is found or timeout is reached.
     while True:
-        dist, rot = box_data(prime_code)
-        go(dist,rot)
-        # The release distance as the value (d_th+0.15) was chosen by preference.
-        # It avoids crashing with the boxes but also places them at a reasonable distance amongst themselves.
-        if dist <= d_th+0.15:
+        update_vision_data()
+        min_distance = 100
+        min_distance_code = None
+
+        # Filter the vision system by the codes provided
+        for code in codes:
+            if code in robot_vision:
+                filtered_robot_vision[code] = robot_vision[code]
+        
+        # Obtain the box with the minimum distance
+        for code, info in filtered_robot_vision.items():
+            distance = info[0]
+            if distance < min_distance:
+                min_distance = distance
+                min_distance_code = code
+
+        # If not a single box is found, turn robot and update vision.
+        if min_distance_code is None:
+            turn(40)
+        else:
             stop()
-            R.release()
-            grabbed = False
-            primel.append(code)
-            return 0
+            return min_distance_code
+        
+        # Exit the program if timout is reached
+        if time.time() - start_time > timeout_seconds:
+            print("No more boxes found. Program has finished.")
+            stop()
+            exit()
+
+def go_to_box(robot_vision, code):
+    '''The go function moves the robot towards the specified box.
+        False: The code is not in the robot_vision list
+        True: The robot has reached the distance threshold'''
+    dist, rot = None, None
+    for box, info in robot_vision.items():
+        if box == code:
+            dist = info[0]
+            rot = info[1]
+    if dist is None or rot is None or code == 0:
+        stop()
+        return False
+
+    # If the angle exceeds the threshold, it will reorient itself
+    if rot < -a_th:
+        turn(-5)
+        #print('Changing Rot',dist, rot)
+    elif rot > a_th:
+        turn(5)
+        #print('Changing Rot',dist, rot) 
+    # With correct angle, the robot will drive to the box.
+    else:
+        turn(0)
+        speed = speed_selector(dist)
+        drive(speed)
+        #print('Changing dist',dist, rot)
+
+    time.sleep(0.1)
+    if dist <= d_th and not has_box:
+        stop()
+        return True
+    elif dist <= d_th + 0.15 and has_box:
+        stop()
+        return True
+
+def robot_action():
+    global boxes_at_goal, has_box, target_box, last_grabbed_box
+
+    if not has_box:
+        R.grab()
+        print("The robot has picked up a box. Target box is:", target_box)
+        has_box = True
+        target_box = 0
+
+    elif has_box:
+        R.release()
+        print("The robot has released the box")
+        boxes_at_goal.append(last_grabbed_box)
+        has_box = False
+        target_box = 0
+        drive(-100, 0.5)
 
 
-#### BEGGINING OF CODE EXECUTION LOOP
+########################
+# CODE EXECUTION
+########################
+        
+initial_boxes = initial_scan() # Dict => code:(dist,ang)
+print('Number of boxes found:', len(initial_boxes))
+
+# Create a list named 'boxes' with the codes of all boxes found during initial scan
+for box in initial_boxes:
+    boxes.append(box)
+
+# Find the initial closest box and set it up as the main box
+main_box = find_closest_box(boxes)
+boxes_at_goal.append(main_box)
+print('The box #', main_box, ' was the closest found, so all boxes will be placed next to it.')
+
+# Initialization of variables with unexisting box code.
+target_box = 0
+last_grabbed_box = 0
+
 
 while True:
-    # The moment the program starts the robot will find the closest box it can see.
-    if grabbed == False:
-        code = find_closest_box()
 
-        # For the first iteration, the closest box found will be added instantly as a prime box.
-        # The code of a prime box will be added to a list called 'primel'
-        # All boxes grabbed will be put together with any box from the list 
-        if len(primel) == 0:
-            primel.append(code)
-            continue
-        
-        # After finding the closest box the robot will go to it and grab it.
-        try:
-            go_grab(code)
-        except:
-            print('Error in grab function')
-            stop()
-            # Most likely the R.see() lost sight of the box, so the code can execute again.
-            continue
-    
-    # After grabbing a box the robot will find any prime box where all other boxes will be put together
-    prime_code = find_prime_box(primel)
+    ### Update the info of the boxes currently being seen
+    update_vision_data()
 
-    # After finding any prime box, the robot will go to it and release its box in a specific number of meters next to it.
-    try:
-        go_release(prime_code, code)
-    except:
-        print('Error in release function')
-        stop()
-        # Most likely the R.see() lost sight of the prime box, so the code can execute again.
-        continue
+    ### Action: Robot to box -> GRAB
+    if not(has_box):
+        go_status = go_to_box(robot_vision, target_box)
 
-    # The robot will go backwards for a few units of distance in order to avoid any collision while looking for next box.
-    drive(-100, 0.5)
-    print("IDs of boxes brought together: ",primel)
+        # The robot has reached a box
+        if go_status == True:
+            robot_action()
+
+        # The robot cannot see any box
+        if go_status == False:
+            print('Re-scaning...')
+            available_boxes = [x for x in boxes if x not in boxes_at_goal]
+            print('Available boxes:', available_boxes)
+            target_box = find_closest_box(available_boxes)
+            last_grabbed_box = target_box
+
+    ### Action: Robot to box -> RELEASE
+    elif (has_box):
+        go_status = go_to_box(robot_vision, target_box)
+
+        # The robot has reached a box
+        if go_status == True:
+            robot_action()
+
+        # The robot cannot see any box
+        if go_status == False:
+            print('Re-scaning...')
+            print('Boxes at goal:', boxes_at_goal)
+            target_box = find_closest_box(boxes_at_goal)
+
 
